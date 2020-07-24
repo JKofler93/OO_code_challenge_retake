078830b70a25f1a27947fb01245f5093147a5e11
github username <github email address>
2020-07-24 19:30:22 -0400

    Done

--- OO_code_challenge_retake/restaurant.rb
+++ OO_code_challenge_retake/restaurant.rb
@@ -1,8 +1,32 @@
 Class Resturant 
-    attr_writer :name, :star_rating
+    
+    attr_accessor :name, :star_rating
 
+    @@all = []
+    
     def initialize(name, star_rating)
         @name = name 
         @star_rating= star_rating
+        @@all << self
+    end 
+
+    def recipes
+        Recipe.all.select {|recipe| recipe.restaurant == self}
+    end 
+
+    def restaurant
+        self.recipes.all {|recipe| recipe.restaurant}
+    end 
+
+    def self.highest_rated
+        Resturant.all.max_by {|restaurant| restaurant.star_rating}
+    end
+
+    def self.find_by_name(name)
+        self.all.find {|restaurant| restaurant.name == self}
+    end 
+
+    def recipe_previews
+        self.recipes.map {|recipe| recipe.description[0...14]}
     end 
 end
